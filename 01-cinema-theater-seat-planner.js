4cd6365261386f267d8b342f0faddf39a60562e1
barua-raj <barua.jovraj@gmail.com>
2025-08-06 23:59:56 +0600

    cinema theater seat planner problem solved

---
+++ 01-cinema-theater-seat-planner.js
@@ -0,0 +1,53 @@
+// Problem 01:
+// =============
+// A seat planner needs to be created for a new cinema theater. The seats in this movie cinema are designed in such a way that if the seat number is divisible by 3 or 5 without any remainder, it will be considered a 'premium' seat. That is, if the seat number is 1, it will be a normal seat, and if the seat number is 3/5, it will be a premium seat.
+
+// Your task is to create a program that will take the total number of seats (totalRows) and print whether each seat is a 'Normal Row' or a 'Premium Row'.
+
+// Example:
+
+// Row 1 - Normal Row
+
+// Row 4 - Premium Row
+
+
+// Problem 02:
+// =============
+// If a seat is normal, the price will be 500 Taka, and if it's a premium seat, the price will be 800 Taka. You have to calculate the total income from one show.
+
+// Example:
+
+// Total Income: 5000 Taka
+
+
+// Problem 01:
+
+const totalRows = 50;
+for (let i = 1; i <= totalRows; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        console.log("Row",i," - Premium Row");
+    }
+    else {
+        console.log("Row",i," - Normal Row");
+    }
+}
+
+console.log("\n");
+
+
+// Problem 02:
+
+const totalRows2 = 8;
+let premiumSeatPrice = 0;
+let normalSeatPrice = 0;
+for (let j = 1; j <= totalRows2; j++) {
+    if (j % 3 === 0 || j % 5 === 0) {
+        premiumSeatPrice = premiumSeatPrice + 800;
+    }
+    else {
+        normalSeatPrice = normalSeatPrice + 500;
+    }
+}
+const totalPrice = premiumSeatPrice + normalSeatPrice;
+console.log(totalPrice);
+
